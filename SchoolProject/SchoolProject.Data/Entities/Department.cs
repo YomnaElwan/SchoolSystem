c638a43625360413bc2a4b5501c01feb8f8f8889
YomnaElwan <yomnaelwan19@gmail.com>
2025-06-22 05:29:46 +0300

    Create Project Architecture

---
+++ SchoolProject/SchoolProject.Data/Entities/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.Data.Entities
+{
+    public class Department
+    {
+        [Key]
+        public int DeptId { get; set; }
+        [StringLength(500)]
+        public string DName { get; set; }
+        public virtual ICollection<Student> Students { get; set; }
+        public virtual ICollection<DepartmentSubject> DepartmentSubjects { get; set; }
+
+    }
+}
---
+++ SchoolProject/SchoolProject.Data/Entities/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.Data.Entities
+{
+    public class Student
+    {
+        [Key]
+        public int StudId { get; set; }
+        [StringLength(200)]
+        public string Name { get; set; }
+        public string Address { get; set; }
+        [StringLength(500)]
+        public string Phone { get; set; }
+        public int? DID { get; set; }
+        [ForeignKey("DeptId")]
+        public virtual Department Department { get; set; }
+
+        public virtual ICollection<StudentSubject> StudentSubjects { get; set; }
+
+
+    }
+}
